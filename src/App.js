0d062eced22e53b10c5b4f0829616e6ce89aa9fe
Abhishek-Mohajan-projects <avimm079@gmail.com>
2023-06-20 22:50:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faTwitter,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
-function App() {
+import image1 from "./images/image1.jpg";
+import "./App.css";
+
+const Card = ({ image, name, designation }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card-image">
+        <img src={image} alt="Hero Image" />
+      </div>
+      <div className="card-body">
+        <div>
+          <h2>{name}</h2>
+          <p>{designation}</p>
+        </div>
+        <div className="card-icons">
+          <a
+            href="https://www.linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="card-icon"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="card-icon"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="card-icon"
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <>
+      <div className="card_header">
+        <h4 className="card_title">Our Trainers</h4>
+        <h1>Instructors</h1>
+      </div>
+      <div className="card-grid">
+        <Card image={image1} name="John Doe" designation="Web Developer" />
+        <Card image={image1} name="Jane Smith" designation="Graphic Designer" />
+        <Card image={image1} name="Mike Johnson" designation="UI/UX Designer" />
+        <Card
+          image={image1}
+          name="Sarah Wilson"
+          designation="Software Engineer"
+        />
+      </div>
+    </>
+  );
+};
 
 export default App;
